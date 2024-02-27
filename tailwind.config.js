20a2d731d3b119e90ebea6a75c528e58c18db6db
Mohammad Jamshidi <mohamadjamshidiiii8@gmail.com>
2024-02-27 04:16:52 +0330

    style: responsive for mobile and tablet

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      'sm': '576px',
-      'lg': '992px',
-      'xl': '1200px',
-      '2xl': '1400px'
-    },
     extend: {
       colors: {
         'primary' : '#4461F2',
